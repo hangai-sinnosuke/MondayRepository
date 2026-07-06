74987175b346436736c3fcb9a31b974c48153372
hangai-sinnosuke <shinn7956@gmail.com>
2026-07-06 09:52:54 +0900

    例題

--- 0706/Main.cpp
+++ 0706/Main.cpp
@@ -1,1 +1,23 @@
 #include<iostream>
+
+using namespace std;
+
+void Add(int& num)
+{
+	num += 10;
+}
+
+int main(void)
+{
+	int number;
+
+	cout << "”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢" << endl;
+
+	cin >> number;
+
+	Add(number);
+
+	cout <<"Œ‹‰Ê‚Í" << number << "‚Å‚·" << endl;
+
+	return  0;
+}
